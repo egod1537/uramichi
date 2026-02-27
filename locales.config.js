e3108a41b8ea96a45c57069470613ee84b8f6b0e
seongjun-yang1537 <seongjun.yang1537@5minlab.com>
2026-02-27 23:09:02 +0900

    chore: localization sheet update

--- locales.config.js
+++ locales.config.js
@@ -1,5 +1,5 @@
 export default {
-  spreadsheetId: '1aBcDeFgHiJkLmNoPqRsTuVwXyZ',
+  spreadsheetId: '1J7a-s-1G_9y5xKW19CJ6_xkUUAOufGh_zmfIyLns0sY',
   sheets: [
     { name: 'common', gid: '0' },
     { name: 'toolbar', gid: '123456' },
