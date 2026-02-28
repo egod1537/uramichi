8c1e9ca092777c2786a246ccf0dd0b14563af8f9
seongjun-yang1537 <seongjun.yang1537@5minlab.com>
2026-02-28 14:33:51 +0900

    update locales

--- locales.config.js
+++ locales.config.js
@@ -1,6 +1,5 @@
 export default {
   spreadsheetId: '1J7a-s-1G_9y5xKW19CJ6_xkUUAOufGh_zmfIyLns0sY',
-  sheets: [{ name: 'common', gid: '0' }],
   languages: ['ko', 'ja', 'en'],
   defaultLanguage: 'ko',
   outputDir: 'src/locales',
